b9186275aac8f909664efdaae972eaf825e87320
Prem Singh <premsingh314.work@gmail.com>
2026-01-28 00:50:46 +0530

    made auto login when create account

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,53 +1,68 @@
 import User from "../models/user.model.js";
 import { GenreateJWT } from "../middlewares/jwt.middleware.js";
 
-export const SignupMethod=async(req,res)=>{
-    const{name,email,password,avatar,phone}=req.body;
-        try{
-            const existing_user = await User.findOne({email:email});
-            if(existing_user){
-                console.log(existing_user);
-                return res.status(409).json({message:"User already exists"});
-            }
-            const new_user=new User({name,email,password,avatar,phone});
-            await new_user.save();
-            res.status(201).json({message:"User created"})
-        }
-        catch(err){
-            console.error("Error during user sign up:", err.message);
-            res.status(500).json({ message: err.message});
-        }
-}
-
-export const LoginMethod = async(req,res)=>{
-    // console.log(req.body);
-    const {email,password}=req.body;
-    try{
-        const existingUser = await User.findOne({email:email}).select('+password');
-        // console.log(existingUser);
-
-        if(existingUser && existingUser.password == password){
-            const payload={
-                _id:existingUser._id,
-                email:email,
-                role:existingUser.role
-            }
-            const jwt_token = GenreateJWT(payload);
-            res.cookie("jwt",jwt_token,{
-            httpOnly: true,                // prevents JS access to cookie
-            secure: process.env.NODE_ENV === "production", // only HTTPS in prod
-            sameSite: "strict"
-            });
-            // console.log(existingUser);
-            return res.status(200).json({ message: "True", token: jwt_token });
-            
-        }
-        else{
-            return res.status(404).json({message:"Invalid email or password"});
-        }
+export const SignupMethod = async (req, res) => {
+  const { name, email, password, avatar, phone } = req.body;
+  try {
+    const existing_user = await User.findOne({ email: email });
+    if (existing_user) {
+      console.log(existing_user);
+      return res.status(409).json({ message: "User already exists" });
     }
-    catch(error){
-        console.error("Error during user sign up:", error.message);
-        res.status(500).json({ message: "Internal server error during sign up." });
+    const new_user = new User({ name, email, password, avatar, phone });
+    await new_user.save();
+
+    const payload = {
+      _id: new_user._id,
+      email: new_user.email,
+      role: new_user.role || "User",
+    };
+
+      const jwt_token = GenreateJWT(payload);
+      
+      res.cookie("jwt", jwt_token, {
+        httpOnly: true, // prevents JS access to cookie
+        secure: process.env.NODE_ENV === "production", // only HTTPS in prod
+        sameSite: "strict",
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+    res.status(201).json({ 
+        message: "User created and Logged in!",
+     });
+  } catch (err) {
+    console.error("Error during user sign up:", err.message);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const LoginMethod = async (req, res) => {
+  // console.log(req.body);
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email: email }).select(
+      "+password",
+    );
+    // console.log(existingUser);
+
+    if (existingUser && existingUser.password == password) {
+      const payload = {
+        _id: existingUser._id,
+        email: email,
+        role: existingUser.role,
+      };
+      const jwt_token = GenreateJWT(payload);
+      res.cookie("jwt", jwt_token, {
+        httpOnly: true, // prevents JS access to cookie
+        secure: process.env.NODE_ENV === "production", // only HTTPS in prod
+        sameSite: "strict",
+      });
+      // console.log(existingUser);
+      return res.status(200).json({ message: "True", token: jwt_token });
+    } else {
+      return res.status(404).json({ message: "Invalid email or password" });
     }
-}
+  } catch (error) {
+    console.error("Error during user sign up:", error.message);
+    res.status(500).json({ message: "Internal server error during sign up." });
+  }
+};
